65077e0076c072a28e2c2f6bf81125224fca364e
jojoDEV5050 <293896750+JojoNeedsPizza@users.noreply.github.com>
2026-06-29 11:56:00 +0200

    ascii and a kind of a menu

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 import json
 import os  # os hilft uns zu prüfen, ob die Datei schon existiert
 import datetime
+from pick import pick
 # No GUI for now!!!!
 # Test Build 1 Ready for Pre- Alpha
 
-from pick import pick
+
 
 def add_idea():
     currentdateandtime = datetime.datetime.now()
-    newidea = 0
     databasetemp = {}
     if os.path.exists("database.json"):
         with open("database.json", "r", encoding="utf-8") as file:
@@ -24,8 +24,6 @@ def add_idea():
 
     countofideas = len(databasetemp)
     coid = countofideas + 1
-    print(str(coid))
-
     nameofidea = input("Enter your Ideas Name: ")
     descriptionofidea = input("Describe this Idea: ")
     nid = nameofidea
@@ -39,8 +37,6 @@ def add_idea():
     "Date and Time": f'{cdnt}'
     }
 
-    print(databasetemp)
-
     if nid == "":
         print("No Idea saved")
 
@@ -50,28 +46,47 @@ def add_idea():
         print("Idea Saved Successfully!")
 
 
+logotext = r"""
 
 
 
+  ######         ##                      ######:                   ##            . ####:              .####.             #####:    ########  ##:  :## 
+  ######         ##                      #######:                  ##            #######:             ######             #######   ########  ##    ## 
+    ##           ##                      ##   :##                  ##            #:.   ##            :##  ##:            ##  :##:  ##        :##  ##: 
+    ##      :###.##   .####:    :####    ##    ##   :####     :###.##                  ##            ##:  :##            ##   :##  ##        :##  ##: 
+    ##     :#######  .######:   ######   ##   :##   ######   :#######                 :#             ##    ##            ##   .##  ##         ## .##  
+    ##     ###  ###  ##:  :##   #:  :##  #######:   #:  :##  ###  ###                 ##             ## ## ##            ##    ##  #######    ##::##  
+    ##     ##.  .##  ########    :#####  ######:     :#####  ##.  .##               .##:             ## ## ##            ##    ##  #######    ##::##  
+    ##     ##    ##  ########  .#######  ##        .#######  ##    ##              .##:              ##    ##            ##   .##  ##         :####:  
+    ##     ##.  .##  ##        ## .  ##  ##        ## .  ##  ##.  .##             :##:               ##:  :##            ##   :##  ##         .####.  
+    ##     ###  ###  ###.  :#  ##:  ###  ##        ##:  ###  ###  ###            :##:         ##     :##  ##:            ##  :##:  ##          ####   
+  ######   :#######  .#######  ########  ##        ########  :#######            ########     ##      ######             #######   ########    ####   
+  ######    :###.##   .#####:    ###.##  ##          ###.##   :###.##            ########     ##      .####.             #####:    ########     ##    
+
+
+                                               Made by JojoNeedsPizza  --Still in developement                                                                                                       
+"""
+
+#print(logotext)
 
 
 # Das Hauptmenü definieren
-titel = "=== IDEAPAD HAUPTMENÜ ===\n(Nutze die Pfeiltasten zum Steuern und drücke ENTER)"
-optionen = ["1. Add a new Idea", "2. Show Ideas", "3. Programm beenden"]
+#titel = "=== IDEAPAD HAUPTMENÜ ===\n(Nutze die Pfeiltasten zum Steuern und drücke ENTER)"
+#optionen = ["1. Add a new Idea", "2. Show Ideas", "3. Programm beenden"]
 
 # Das Menü starten. 'option' hält den Text, 'index' die Position (0, 1, 2)
-option, index = pick(optionen, titel, indicator="=>", default_index=0)
+#option, index = pick(optionen, titel, indicator="=>", default_index=0)
 
 # Auswerten, was gedrückt wurde anhand des Index (0-basiert)
-if index == 0:
-    print("\n[Du hast 'Neue Idee hinzufügen' ausgewählt]")
+#if index == 0:
+#    print("\n[Du hast 'Neue Idee hinzufügen' ausgewählt]")
     # Hier kommt dein Input- und Speicher-Code hin
-    add_idea()
+#    add_idea()
 
-elif index == 1:
-    print("\n[Du hast 'Alle Ideen anzeigen' ausgewählt]")
+#elif index == 1:
+#    print("\n[Du hast 'Alle Ideen anzeigen' ausgewählt]")
 
-elif index == 2:
-    print("Auf Wiedersehen!")
+#elif index == 2:
+#    print("Auf Wiedersehen!")
 
-
+add_idea()
