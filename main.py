07b9314a02f4acb6658e9d082afc69721789f7af
jojoDEV5050 <293896750+JojoNeedsPizza@users.noreply.github.com>
2026-06-29 10:49:06 +0200

    Debugging 🫩

--- main.py
+++ main.py
@@ -17,12 +17,14 @@ newidea = input("Enter your Idea: ")
 nid = newidea
 
 
-Idea + coid = {
-
-
-
-
+# 3. Deine Formel angewendet: Wir fügen das Dictionary direkt unter der Zahl ein
+["neues_dict" + str(coid)] = {
+    "Name of Idea": "Smart Trash Can",
+    "Description": "Ein Mülleimer, der Chips-Tüten automatisch scannt und nachbestellt."
 }
+
+# Wenn du jetzt das Haupt-Dictionary druckst:
+print(datenbank)
 
 if nid == "":
     print("No Idea saved")
