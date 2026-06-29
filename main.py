d0ef7f33402e0a1b67343e0e44d75a4f7656ebdb
jojoDEV5050 <293896750+JojoNeedsPizza@users.noreply.github.com>
2026-06-29 12:53:51 +0200

    fixing stuff

--- main.py
+++ main.py
@@ -23,7 +23,9 @@ logotext = r"""
   ######    :###.##   .#####:    ###.##  ##          ###.##   :###.##            ########     ##      .####.             #####:    ########     ##    
 
 
-                                               Made by JojoNeedsPizza  --Still in developement                                                                                                       
+                                               Made by JojoNeedsPizza  --Still in developement
+                                                GitHub: https://github.com/JojoNeedsPizza/IdeaPad
+                                                Profile: https://github.com/JojoNeedsPizza                                                                                                       
 """
 main_menu_ui_title = r"""
 ---------------------------------------------------
@@ -43,7 +45,9 @@ show_all_ideas = r"""
 ---------------------------------------------------
 """
 
-
+def clear_terminal():
+    # Führt den passenden Terminal-Befehl für dein Betriebssystem aus
+    os.system('cls' if os.name == 'nt' else 'clear')
 
 #---------------------------------------------------
 #|              Add a new Idea                     |
@@ -81,16 +85,44 @@ def add_idea():
 
     if nid == "":
         print("No Idea saved")
+        input("\nPress ENTER to return to the main menu...")
+        return
 
     else:
        with open("database.json", "w") as file:
         json.dump(databasetemp, file, indent=4)
         print("Idea Saved Successfully!")
+        input("\nPress ENTER to return to the main menu...")
+
+
+
 
 #---------------------------------------------------
 #|                Show all Ideas                   |
 #---------------------------------------------------
 def show_ideas():
+    print(show_all_ideas)
+    databasetemp = {}
+    if os.path.exists("database.json"):
+        with open("database.json", "r", encoding="utf-8") as file:
+            try:
+                databasetemp = json.load(file)
+            except json.JSONDecodeError:
+                pass
+
+    # 2. Prüfen, ob die Datenbank leer ist
+    if not databasetemp:
+        print("Es wurden noch keine Ideen gespeichert! Schnapp dir einen Kaffee und werd kreativ. ☕")
+        return
+
+    # 3. Jede Idee einzeln und hübsch formatiert ausgeben
+    for idea_id, data in databasetemp.items():
+        print(f"💡 [{idea_id}]")
+        print(f"   Name:        {data.get('Name of Idea', 'Unbekannt')}")
+        print(f"   Beschreibung:{data.get('Description', 'Keine Beschreibung')}")
+        print(f"   Gespeichert: {data.get('Date and Time', '-')}")
+        print("-" * 35)  # Trennlinie zwischen den Ideen
+    input("\nPress ENTER to return to the main menu...")
 
 
 
@@ -107,15 +139,56 @@ def show_ideas():
 #---------------------------------------------------
 
 def main_menu():
- print(main_menu_ui_title)
- titel = "\n(Use the Arrow Keys to steer and press Enter to select)"
- optionen = ["1. Add a new Idea", "2. Show Ideas", "3. Exit Program"]
- option, index = pick(optionen, titel, indicator="=>", default_index=0)
- if index == 0:
-    add_idea()
- elif index == 1:
-    show_ideas()
- elif index == 2:
-    print("Goodbye!")
+    while True:
+        clear_terminal()
 
 
+        titel = r"""
+        
+
+
+  ######         ##                      ######:                   ##            . ####:              .####.             #####:    ########  ##:  :## 
+  ######         ##                      #######:                  ##            #######:             ######             #######   ########  ##    ## 
+    ##           ##                      ##   :##                  ##            #:.   ##            :##  ##:            ##  :##:  ##        :##  ##: 
+    ##      :###.##   .####:    :####    ##    ##   :####     :###.##                  ##            ##:  :##            ##   :##  ##        :##  ##: 
+    ##     :#######  .######:   ######   ##   :##   ######   :#######                 :#             ##    ##            ##   .##  ##         ## .##  
+    ##     ###  ###  ##:  :##   #:  :##  #######:   #:  :##  ###  ###                 ##             ## ## ##            ##    ##  #######    ##::##  
+    ##     ##.  .##  ########    :#####  ######:     :#####  ##.  .##               .##:             ## ## ##            ##    ##  #######    ##::##  
+    ##     ##    ##  ########  .#######  ##        .#######  ##    ##              .##:              ##    ##            ##   .##  ##         :####:  
+    ##     ##.  .##  ##        ## .  ##  ##        ## .  ##  ##.  .##             :##:               ##:  :##            ##   :##  ##         .####.  
+    ##     ###  ###  ###.  :#  ##:  ###  ##        ##:  ###  ###  ###            :##:         ##     :##  ##:            ##  :##:  ##          ####   
+  ######   :#######  .#######  ########  ##        ########  :#######            ########     ##      ######             #######   ########    ####   
+  ######    :###.##   .#####:    ###.##  ##          ###.##   :###.##            ########     ##      .####.             #####:    ########     ##    
+
+
+                                               Made by JojoNeedsPizza  --Still in developement
+                                                GitHub: https://github.com/JojoNeedsPizza/IdeaPad
+                                                Profile: https://github.com/JojoNeedsPizza  
+                                                
+                                                
+                                                
+                                                ---------------------------------------------------
+                                                |                  Main Menu                      |
+                                                ---------------------------------------------------
+                                                
+                                                (Use the Arrow Keys to steer and press Enter to select)
+        """
+        optionen = ["1. Add a new Idea", "2. Show Ideas", "3. Exit Program"]
+        option, index = pick(optionen, titel, indicator="=>", default_index=0)
+        if index == 0:
+          add_idea()
+        elif index == 1:
+           show_ideas()
+        elif index == 2:
+           print("Goodbye!")
+           break
+
+
+
+# Show Ideas Func = show_ideas()
+# Main Menu Func = main_menu()
+# Add New Idea Func = add_idea()
+
+# Dieser Befehl sorgt dafür, dass das Menü beim Starten anspringt
+if __name__ == "__main__":
+    main_menu()
