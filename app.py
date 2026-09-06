a99bc3d6928f9c5bdb5128d95428527c663630c5
JojoNeedsPizza <johannes.faizade@gmx.de>
2026-09-07 01:10:40 +0200

    Core Beta 1

--- app.py
+++ app.py
@@ -1,11 +1,30 @@
 import datetime
 import tkinter
-
+import json
+import sys
+import os
 print("Got an Idea? Write it down")
 
 idea = input("Enter your idea: ")
 description = input("Describe your idea: ")
-Date = datetime.datetime.now()
+date = datetime.datetime.now()
 
-idea = {"Idea": idea, "Description": description }
+ideadict = {"Idea": idea, "Description": description}
+
+
+print(ideadict)
+if os.path.exists("./result.json") and os.path.getsize("./result.json") > 0:
+    with open('result.json', 'r') as read_file_save:
+        savefileforstopoverwrite = json.load(read_file_save)
+
+        writefiletosave =  str(savefileforstopoverwrite) + "\n" + str(ideadict)
+
+        print(writefiletosave)
+
+        with open('result.json', 'w') as fp:
+            json.dump(writefiletosave, fp)
+
+else:
+    with open('result.json', 'w') as fp:
+            json.dump(ideadict, fp)
 
