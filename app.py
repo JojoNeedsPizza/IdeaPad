8d3d01e0045f5ee3137386de45c3ecfa32d1724d
jojoDEV5050 <293896750+JojoNeedsPizza@users.noreply.github.com>
2026-09-07 14:59:39 +0200

    Save for transfer to Laptop

--- app.py
+++ app.py
@@ -3,19 +3,29 @@ import tkinter
 import json
 import sys
 import os
+
+
+file_path = "results.json"
+
+
+if os.path.exists(file_path):
+    
+
+
+
 print("Got an Idea? Write it down")
 
 idea = input("Enter your idea: ")
 description = input("Describe your idea: ")
 date = datetime.datetime.now()
-
-ideadict = {"Idea": idea, "Description": description}
+idea_number = total_lines + 1
+ideadict = {"ID":idea_number, "Idea": idea, "Description": description}
 json_string = json.dumps(ideadict, indent=2)
 
 print(json_string)
 
 print(ideadict)
-if os.path.exists("./result.json") and os.path.getsize("./result.json") > 0:
+if os.path.exists(file_path) and os.path.getsize(file_path) > 0:
     with open('result.json', 'r') as read_file_save:
         savefileforstopoverwrite = json.load(read_file_save)
 
@@ -24,10 +34,10 @@ if os.path.exists("./result.json") and os.path.getsize("./result.json") > 0:
 
         writetojson = json.dumps(writefiletosave, indent=2)
 
-        with open('result.json', 'w') as fp:
+        with open(file_path, 'w') as fp:
             json.dump(writetojson, fp, indent=2)
 
 else:
-    with open('result.json', 'w') as fp:
+    with open(file_path, 'w') as fp:
             json.dump(json_string, fp, indent=2)
 
